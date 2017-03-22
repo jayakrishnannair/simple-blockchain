2515bbb2e95319fdb7c3887b3a5f09cafbc53eee
Jayakrishnan Nair <jnair@lendingclub.com>
2017-03-21 21:59:10 -0700

    Initial Commit

---
+++ src/main/java/org/shooonya/Block.java
@@ -0,0 +1,18 @@
+package org.shooonya;
+
+import java.util.Date;
+
+/**
+ * Represents a block in the chain
+ *
+ * @author jnair
+ * @since 3/21/17
+ */
+public class Block {
+
+    private int index;
+
+    private String hash;
+
+    private Date timestamp;
+}
